3fe3948dd25e09494c68378192541158c94acb86
Olga Korskova <o-korskova@yandex.ru>
2022-10-09 18:55:41 +0300

    Modified project

--- src/test/java/CardDelivery.java
+++ src/test/java/CardDelivery.java
@@ -1,4 +1,3 @@
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -6,38 +5,32 @@ import org.openqa.selenium.Keys;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.regex.Pattern;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.files.DownloadActions.click;
-import static java.awt.SystemColor.menu;
 
 
 public class CardDelivery {
     String generateDate(int days) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-    };
+    }
 
     @BeforeEach
     void setup() {
-        String date = generateDate(3);
+        open("http://localhost:7777");
     }
-
 
     @Test
     void shouldTestValidForm() {
-        open("http://localhost:7777");
-        $("[data-test-id='city']").setValue("Москва");
-        $(".class(menu-item__control)").click();
-        $("[data-test-id='data']").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME),Keys.DELETE);
-        $("[data-test-id='data']").sendKeys(Keys.ENTER);
-        $("[data-test-id='name']").setValue("Петров Иван");
-        $("[data-test-id='phone']").setValue("+79998888888");
-        $("[data-test-id='agreement']").click();
+        String date = generateDate(3);
+        $("[data-test-id=city] input").setValue("Москва");
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue("Иванов Петр");
+        $("[data-test-id=phone] input").setValue("+79998888888");
+        $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Запланировать")).click();
-        $(byText("Встреча успешно запланирована на ")).shouldBe(visible, Duration.ofSeconds(10));
+        $("[data-test-id=success-notification]").shouldHave(exactText("Успешно! " + "Встреча успешно запланирована на " + date), Duration.ofSeconds(10)).shouldBe(visible);
     }
 
 }
